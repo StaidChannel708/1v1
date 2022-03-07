16acc4e87d606bce25ece65901ba8c77ac7989ec
StaidChannel708 <66675378+StaidChannel708@users.noreply.github.com>
2022-03-07 17:18:19 +0000

    oh god

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 var url =  'https://staidchannel708.github.io/1v1/';
 window.open(url, '_blank');
+window.open(url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
